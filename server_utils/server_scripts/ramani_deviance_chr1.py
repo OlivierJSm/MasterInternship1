5ecfcff3f5472898fcbb1291e25e3269270fd7dc
Olivier Smeets <osmeets@mbscourse.science.ru.nl>
2026-05-11 13:02:31 +0000

    Added additional server scripts

---
+++ server_utils/server_scripts/ramani_deviance_chr1.py
@@ -0,0 +1,41 @@
+### Setting directories
+data_dir = '/mbshome/osmeets/MasterInternship1/Data/ramani_cool_files' # Importing was not working with relative root+'..' approach, so replace with local data
+metadata_dir = f"{data_dir}/cell_line_mapping.csv"
+### Imports
+from hicdatautils import hic_ot_optim, import_cool_dir, subset_clr_data
+import pandas as pd
+
+### Importing coolers and metadata
+clrs = import_cool_dir(data_dir)
+metadata_df = pd.read_csv(metadata_dir)
+
+### Setting parameters
+save_dir = '/mbshome/osmeets/MasterInternship1/Generated Data/Server'
+count = None
+top = 1500
+reg_m = 0.1
+
+### Subset
+print("Starting subsetting\n")
+unbalanced_subset = subset_clr_data(
+    data_dir,
+    metadata_df,
+    count,
+    iqr_rule = 1.5
+)
+print("Finished subsetting\n")
+
+### OT
+print("Starting OT\n")
+unbalanced_results = hic_ot_optim(
+    unbalanced_subset,
+    "chr1",
+    "deviance",
+    "max",
+    unbalanced=reg_m,
+    top_contacts=top
+)
+print("Finished OT\n")
+
+### Saving
+unbalanced_results.to_csv(f"{save_dir}/ramani_deviance_chr1_count={count}_top={top}_reg_m={reg_m}.csv")
